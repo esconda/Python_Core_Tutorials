fc7ab23b8733147dde0aef28b8736e815bd6a05a
esconda <pm_brk_pm@hotmail.com>
2022-03-14 16:40:48 +0500

    19.Functions are added

--- 19.Functions/Functions.py
+++ 19.Functions/Functions.py
@@ -1,133 +1,187 @@
 from fileinput import filename
 from inspect import getfile
+from math import factorial
 import mmap
 import os
 from sympy import content
 import shutil
 
 #Author: Burak Dogancay
+""" Functions in Python provide organized, reusable and modular code to perform a set of specific actions. Functions
+simplify the coding process, prevent redundant logic, and make the code easier to follow. This topic describes the
+declaration and utilization of functions in Python.
+Python has many built-in functions like print(), input(), len(). Besides built-ins you can also create your own
+functions to do more specific jobs—these are called user-defined functions. """
+def simpleFunction():
+    print("This is a simple function without arguments")
+    print("---------------------")
 
-def getFile(pFileName):
-    location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) #it gets the folder location
-    fileName= os.path.join(location,pFileName)#add file name to folder location
-    return fileName
+def simpleFunctArg(arg="Default argument"):#here we define default argument for arg
+    print("This is a simple function with argument : ", arg)
+    print("---------------------")
+    
+def funcwithmanyArg(*args):
+    print("Arbitrary Number of arguments")
+    for i in args:
+        print(i)
+    print("---------------------")
 
-def fileModes():
-    #'r' - reading mode. The default. It allows you only to read the file, not to modify it. When using this mode the file must exist.
-    #'w' - writing mode. It will create a new file if it does not exist, otherwise will erase the file and allow you towrite to it.
-    #'a' - append mode. It will write data to the end of the file. It does not erase the file, and the file must exist for this mode.
-    #'rb' - reading mode in binary. This is similar to r except that the reading is forced in binary mode. This isalso a default choice.
-    #'r+' - reading mode plus writing mode at the same time. This allows you to read and write into files at thesame time without having to use r and w.
-    #'rb+' - reading and writing mode in binary. The same as r + except the data is in binary
-    #'wb' - writing mode in binary. The same as w except the data is in binary.
-    #'w+' - writing and reading mode. The exact same as r + but if the file does not exist, a new one is made.Otherwise, the file is overwritten.
-    #'wb+' - writing and reading mode in binary mode. The same as w + but the data is in binary.
-    #'ab' - appending in binary mode. Similar to a except that the data is in binary.
-    #'a+' - appending and reading mode. Similar to w + as it will create a new file if the file does not exist.Otherwise, the file pointer is at the end of the file if it exists.
-    #'ab+' - appending and reading mode in binary. The same as a + except that the data is in binary.
-    try:
-        with open(getFile('something.txt'),"r") as readFile:
-            print("Files is available")
-            print("---------------------")
-            readFile.close()
-    except FileExistsError:
-        print("File can not be opened")
-        
-def readByLine():
-    if os.path.isfile(getFile("something.txt")):
-        #Read by line methode  
-        with open(getFile("something.txt"),"r") as readFile:
-            for line in readFile:
-                print("Current Line : ",line)
-            readFile.close()
-        #---------------------
-        
-        #Read by Line alternative methode
-        with open(getFile("something.txt"),"r") as readFileAlt:
-            while True:
-                currentLine = readFileAlt.readline()
-                if currentLine== '':
-                    break
-                print("Current Line Alternative :",currentLine)
-            readFileAlt.close()
-        #---------------------
+def functwithkeyargs(**kwargs):
+    print("Arbitrary Number of keyword arguments")
+    for types,value in kwargs.items():
+        print(types,value)
     print("---------------------")
     
-def writingToFile():
-    if os.path.isfile(getFile("something.txt")):
-        with open(getFile("something.txt"),"w") as writeFile:
-            writeFile.write("Mercedes ")
-            writeFile.write("Bmw ")
-            writeFile.write("Volskwagen ")
-            writeFile.write("Toyota ")
-            writeFile.write("Tesla ")
-        writeFile.close()
-    
-def iterateAllFiles():
-    #To iterate all files, including in sub directories, use os.walk
-    """ print("Directory folder : ",os.getcwd())
-    for root, folders, files in os.walk(os.getcwd()):
-        print ("Root : " , root)
-        for filename in files:
-            print("Files : ", filename) """
-            
-def fullContentFile():
-    if os.path.isfile(getFile("something.txt")):
-        with open(getFile("something.txt")) as readFile:
-            contentFile = readFile.read()
-            print("Content of a File : ",contentFile) 
-            readFile.close()
-    print("---------------------")
-    
-def randomFileAccess():
-    if os.path.isfile(getFile("something.txt")):
-        with open(getFile("something.txt"),'r+') as readFile:
-            mmVar = mmap.mmap(readFile.fileno(), 0)
-            print("Print Characters : ", mmVar[5:10])
-            print("Print The Line Starting from current pos: ",mmVar.readline())
-            print("Returning position to the beginnig of the file : ", mmVar.seek(0))
-        print("---------------------")
-        mmVar.close()    
-        readFile.close()
+def mixFunction(var = "Default var",*args,**kwargs):
+    print("Mixed function with arguments")
+    print("Variable is : ",var)
+    for i in args:
+        print(i)
         
-def copyDirectoryTree():
-    print("Copy directory to destination")
-    #source='//192.168.1.2/Daily Reports'
-    #destination='D:\\Reports\\Today'
-    #shutil.copytree(source, destination) # copy directory from src to dst
-    #shutil.copyfile(src,dts) #copy file from src to dst
+    for types,value in kwargs.items():
+        print(types,value)
+    print("---------------------")
+
+def optionalArg(action='nothing'):
+    #Optional arguments can be defined by assigning (using =) a default value to the argument-name:
+    print("Optional Argument is : ",action)
+    print("---------------------")
+    return action  
+    
+def lambdaFunct():
+    lambdaEx = lambda: "Hello World"
+    strip_and_upper_case = lambda str: str.strip().upper()#Lambda takes arguments "str"
+    argsLambda = lambda x, *args, **kwargs:(x,args,kwargs) 
+    sortWithLambda = sorted( [" foo ", " bAR", "BaZ "], key=lambda s: s.strip().upper())
+    mapWithLambda = sorted( map( lambda s: s.strip().upper(), [" foo ", " bAR", "BaZ "]))
+    
+    #List with Lambda
+    listWithLambda = [3, -4, -2, 5, 1, 7]
+    opLambdaList = list( filter( lambda x: x>0, listWithLambda))
+    opLambdaList1 = list( map( lambda x: abs(x), listWithLambda))
+    opLambdaList2 = sorted( listWithLambda, key=lambda x: abs(x))
+    
+    #Recursive Lambda
+    lambda_factorial = lambda i:1 if i==0 else i*lambda_factorial(i-1)
+    
+    print("LambdaEx output : ", lambdaEx)
+    print("Lambda with arguments : ",strip_and_upper_case("Strings") )
+    print("Lambda with arguments and keyword arguments : ",argsLambda('hello', 'world', world='world'))
+    print("Sorting with Lambda : ",sortWithLambda)
+    print("Map with Lambda : ",mapWithLambda)
+    print("List with Lambda : ",opLambdaList)
+    print("List with Lambda : ",opLambdaList1)
+    print("List with Lambda : ",opLambdaList2)
+    print("Recursive Lambda:", lambda_factorial(4))
     print("---------------------")
     
+def mutableFunction(passedList):
+    #argument (actual parameter): the actual variable being passed to a function;
+    #parameter (formal parameter): the receiving variable that is used in a function.
+    #In Python, arguments are passed by assignment (as opposed to other languages, where arguments can be
+    #passed by value/reference/pointer)
+    #Immutable: Integers, strings, tuples, and so on. All operations make copies.
+    #Mutable: Lists, dictionaries, sets, and so on. Operations may or may not mutate
+    passedList[0] = 10
+    print("Mutable var : ",passedList)
+    print("---------------------")
+    
+def returnVarFunc(arg):
+    print("Returned Function Variable : ",arg)
+    print("---------------------")
+    return arg
+
+def closureFunct(x):
+    #Closures in Python are created by function calls. Here, the call to makeInc creates a binding for x that is referenced
+    #inside the function inc. Each call to makeInc creates a new instance of this function, but each instance has a link to a
+    #different binding of x
+    def inc(y):
+        return y + x
+    return inc
+
+def nestedFunction(var):
+    def fibonacci(n):
+        def step(a,b):
+            return b,a+b
+        a,b = 0,1
+        for i in range(n):
+            a,b = step(a,b)
+        return a
+    fibonacci(var)
+
+def recursiveFunctionsFact(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
 def main():
+    #ABOUT FUNCTION ARGUMENTS
+    #arg1, ..., argN Regular arguments
+    #*args Unnamed positional arguments
+    #kw1, ..., kwN Keyword-only arguments
+    #**kwargs The rest of keyword arguments
     
-    # Call File Modes
-    fileModes()
+    #Call Simple Function Without arguments
+    simpleFunction()
     #----------------
     
-    # Reading File By Name
-    readByLine()
+    #Call Simple Function with Argument
+    simpleFunctArg("My Argument")
     #----------------
     
-    #Iterate All Files
-    iterateAllFiles()
+    #Call function with many arguments
+    funcwithmanyArg(1,2,3)
+    
+    myList = ["TB2","AKINCI","MIUS"]
+    funcwithmanyArg(*myList)# Calling it with list of values, * expands the list
     #----------------
     
-    #Iterate All Files
-    fullContentFile()
+    #Call function with many keyword arguments
+    functwithkeyargs(type1="TB2", type2="AKINCI", type3="MIUS")
+    
+    my_dict = {'car': "toyota", 'plane':"airbus"}
+    functwithkeyargs(**my_dict)#Calling it with dictionary
     #----------------
     
-    #Write File
-    writingToFile()
+    #Mixed function with arguments and keyword arguments
+    var = "Defined Argument"
+    myList_args = ["TB2","AKINCI","MIUS"]
+    my_dict_keyargs = {'car': "toyota", 'plane':"airbus"}
+    mixFunction(var,*myList_args,**my_dict_keyargs)
     #----------------
     
-    #Random File Access
-    randomFileAccess()
+    #Defining a function with optional arguments
+    optionalArg("Heyo")
+    optionalArg(action="pofff")
+    optionalArg()
     #----------------
     
-    #Copy Directory Tree
-    copyDirectoryTree()
+    #Lambda Functions
+    lambdaFunct()
     #----------------
-
+    
+    #Argument passing and mutability
+    myList = [1,2,3,4,5]
+    mutableFunction(myList)
+    #----------------
+    
+    #Return Function Variable
+    returnVarFunc(10)
+    #----------------
+    
+    #Closure 
+    closureFunct(30)
+    #----------------
+    
+    #Nested Functions
+    nestedFunction(5)
+    #----------------
+    
+    #Recursive Functions
+    recursiveFunctionsFact(15)
+    #----------------
+    
 
 if __name__ == '__main__':
     main()
