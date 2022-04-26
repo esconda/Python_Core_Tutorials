e5b33e45f1d389d87c04d494a58951133d30aae7
esconda <pm_brk_pm@hotmail.com>
2022-04-26 23:02:25 +0500

    67.Serial Com impl is added

---
+++ 67.SerialCommunication/SerialCommunication.py
@@ -0,0 +1,18 @@
+# Author: Burak Dogancay
+import serial 
+def initSerialDevice():
+  ser = serial.Serial(')
+  
+
+def main():
+ # Call sub class 
+ subClass= SubClass()
+ subClass.printSomething()
+ subClass.calculation(5)
+ 
+  
+
+  
+
+if __name__ == '__main__':
+    main()
